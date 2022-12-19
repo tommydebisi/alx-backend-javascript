8001014ba837575e7d77d943ef5626cc12d9644a
tommydebisi <tommydebisi@gmail.com>
2022-12-19 19:49:40 +0100

    :construction: tasks 0-12 added

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,12 @@
+export default function createReportObject(employeesList) {
+  const object = {
+    allEmployees: {
+      ...employeesList,
+    },
+    getNumberOfDepartments(employeesList) {
+      return Object.keys(employeesList).length;
+    },
+  };
+
+  return object;
+}
