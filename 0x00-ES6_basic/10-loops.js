274e1536d3a30718eca6f87763005928fba4b6d5
tommydebisi <tommydebisi@gmail.com>
2022-12-20 02:10:22 +0100

    :seedling: return new arr

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (const [idx, value] of array.entries()) {
-    // eslint-disable-next-line no-param-reassign
-    array[idx] = appendString + value;
+  const arr = [];
+  for (const value of array) {
+    arr.push(appendString + value);
   }
 
-  return array;
+  return arr;
 }
