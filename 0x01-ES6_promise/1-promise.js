3df465029b43604d6e891cad8746d8c2fd529956
tommydebisi <tommydebisi@gmail.com>
2022-12-25 21:27:39 +0100

    :seedling: caught errors when rejected

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,14 +1,16 @@
-function getFullResponseFromAPI(success) {
-  return new Promise((res, rej) => {
+export default function getFullResponseFromAPI(success) {
+  const errMessage = new Error('The fake API is not working currently');
+  const apiProm = new Promise((res, rej) => {
     if (success) {
       res({
         status: 200,
         body: 'success',
       });
     } else {
-      rej(new Error('The fake API is not working currently'));
+      rej(errMessage);
     }
   });
-}
 
-export default getFullResponseFromAPI;
+  apiProm.catch(() => {});
+  return apiProm;
+}
