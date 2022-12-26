572330551ccd94e9b5e9a25ccd3fb6ae653496b6
tommydebisi <tommydebisi@gmail.com>
2022-12-26 06:10:00 +0100

    :construction: task3 added

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,9 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  const promList = Promise.all([uploadPhoto(), createUser()]);
+
+  promList.then((value) => {
+    console.log(value[0].body, value[1].firstName, value[1].lastName);
+  });
+}
