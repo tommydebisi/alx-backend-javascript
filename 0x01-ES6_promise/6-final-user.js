83b9072eae197cc42d460b6a58ba0dd54c73700d
tommydebisi <tommydebisi@gmail.com>
2022-12-26 10:22:08 +0100

    :construction: task 6 added

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,17 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const statusArr = [];
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((prom) => {
+      prom.forEach((result) => {
+        statusArr.push({
+          status: result.status,
+          value: result.value ? result.value : result.reason,
+        });
+      });
+
+      return statusArr;
+    });
+}
