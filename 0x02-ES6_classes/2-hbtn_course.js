fabaecf63a1f48e396150355771e0d7bce931de6
tommydebisi <tommydebisi@gmail.com>
2022-12-27 23:03:02 +0100

    :construction: task2 added

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,48 @@
+/* eslint-disable no-underscore-dangle */
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this.name = name;
+    this.length = length;
+    this.students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    if (!(typeof newName === 'string')) {
+      throw TypeError('Name must be a string');
+    }
+
+    this._name = newName;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(newLength) {
+    if (!(typeof newLength === 'number')) {
+      throw TypeError('Length must be a number');
+    }
+
+    this._length = newLength;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(newStudents) {
+    if (!(newStudents instanceof Array)) {
+      throw TypeError('Students must be an array of strings');
+    }
+
+    if (!(newStudents.every((val) => typeof val === 'string'))) {
+      throw TypeError('Students must be an array of strings');
+    }
+
+    this._students = newStudents;
+  }
+}
