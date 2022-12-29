dcb27d311d717c85ccde84a4b8413af488bbbb76
tommydebisi <tommydebisi@gmail.com>
2022-12-29 02:46:18 +0100

    :seedling: reverted to using first and last name

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -33,7 +33,7 @@ export class StudentHolberton {
   }
 
   get fullStudentDescription() {
-    return `${this.fullName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
+    return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
   }
 }
 
