6ef23b8fa9700a4e77b9a9dc7d64394e61b2cc40
tommydebisi <tommydebisi@gmail.com>
2023-01-04 00:43:42 +0100

    :seedling: checked all edge cases

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,9 @@
 export default function cleanSet(set, startString) {
-  if (!startString) { return ''; }
+  if (!startString || !(set instanceof Set) || typeof startString !== 'string') { return ''; }
 
   const strArr = [];
   for (const elem of set) {
-    if (elem.startsWith(startString)) {
+    if (typeof elem === 'string' && elem.startsWith(startString) && elem !== startString) {
       strArr.push(elem.slice(startString.length));
     }
   }
