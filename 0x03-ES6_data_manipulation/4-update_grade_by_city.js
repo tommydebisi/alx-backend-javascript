24abc9c42d145ff18637eb0dcbe2097e4410d6e0
tommydebisi <tommydebisi@gmail.com>
2023-01-04 00:29:35 +0100

    :seedling: added grade to object

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,18 +1,13 @@
 export default function updateStudentGradeByCity(getListStudents, city, newGrades) {
   if (!(Array.isArray(getListStudents)) && !(Array.isArray(newGrades))) { return []; }
 
-  const studsCityArr = getListStudents.filter((val) => val.location === city);
+  return getListStudents.filter((val) => val.location === city)
+    .map((studObj) => {
+      const gradeValArr = newGrades.filter((gradeObj) => gradeObj.studentId === studObj.id);
 
-  return studsCityArr.map((studObj) => {
-    const gradeValArr = newGrades.filter((gradeObj) => gradeObj.studentId === studObj.id);
-
-    const newStudObj = studObj;
-    if (gradeValArr[0]) {
-      Object.assign(newStudObj, gradeValArr[0]);
-    } else {
-      newStudObj.grade = 'N/A';
-    }
-
-    return newStudObj;
-  });
+      return {
+        ...studObj,
+        grade: gradeValArr[0] ? gradeValArr[0].grade : 'N/A',
+      };
+    });
 }
