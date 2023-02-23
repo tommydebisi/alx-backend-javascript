43404585d48d8e16b891957c28fd9c56b40f4bb9
tommydebisi <tommydebisi@gmail.com>
2023-02-23 17:24:35 +0100

    :seedling: task1

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,13 @@
+process.stdin.setEncoding('utf-8')
+console.log('Welcome to Holberton School, what is your name?')
+process.stdin.on('readable', () => {
+  const data = process.stdin.read();
+
+  if (data) {
+    console.log(`Your name is: ${data}`)
+  }
+})
+
+process.stdin.on('end', () => {
+  console.log('This important software is now closing');
+})
