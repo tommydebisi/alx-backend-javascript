ba7de6bb4ce8a5a9e9dd93fe0fca7b279c25966d
tommydebisi <tommydebisi@gmail.com>
2023-02-24 12:31:34 +0100

    :construction: http server

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,11 @@
+/* eslint-disable jest/require-hook */
+const http = require('http');
+
+const app = http.createServer((req, res) => {
+  res.write('Hello Holberton School!');
+  res.end();
+});
+
+app.listen(1245);
+
+module.exports = app;
