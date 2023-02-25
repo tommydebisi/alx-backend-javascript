4594ac533b937888c3da0eb6ac2ad4cb407b803f
tommydebisi <tommydebisi@gmail.com>
2023-02-25 11:35:36 +0100

    :seedling: test arranged well

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,24 +2,19 @@ const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
 describe('function to calculate addition of whole numbers', function() {
-  it('should give correct addition', function() {
+  it('should give correct answer for whole numbers', function() {
     assert.strictEqual(calculateNumber(1, 3), 4);
   });
-});
 
-describe('Function to calculate addition of a float number', function() {
-  it('should give correct addition', function() {
+  it('should round second argument', function() {
     assert.strictEqual(calculateNumber(1, 3.7), 5);
   });
-});
 
-describe('Function to calculate addition of two float numbers', function() {
-  it('should give correct addition', function() {
+  it('should round both arguments', function() {
     assert.strictEqual(calculateNumber(1.2, 3.7), 5);
   });
-});
-describe('Function to get correct addition of float numbers', function() {
-  it('should give correct addition', function() {
+
+  it('should give correct output for rounded numbers', function() {
     assert.strictEqual(calculateNumber(1.5, 3.7), 6);
   });
 });
