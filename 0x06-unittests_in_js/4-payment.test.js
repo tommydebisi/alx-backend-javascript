6d4e24937d789578407c3da836f4a8505ca424be
tommydebisi <tommydebisi@gmail.com>
2023-02-26 15:58:41 +0100

    :seedling: used calledOnce with

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -66,7 +66,7 @@ describe('sendPaymentRequestToApi', function () {
     const consoleSpy = sinon.spy(console, 'log');
     sendPaymentRequestToApi(100, 20);
 
-    expect(stub.calledWith('SUM', 100, 20)).to.be.true;
+    expect(stub.calledOnceWith('SUM', 100, 20)).to.be.true;
     expect(consoleSpy.calledWith('The total is: 10')).to.be.true;
 
     stub.restore();
