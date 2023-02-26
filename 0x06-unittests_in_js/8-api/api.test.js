0e69d82f4121a3780b04ff0dc217f6b3caf90955
tommydebisi <tommydebisi@gmail.com>
2023-02-27 00:34:34 +0100

    :seedling: split test cases

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -2,9 +2,9 @@ const expect = require('chai').expect;
 const request = require('request');
 
 describe('API test from `api.js`', function() {
-  it('Give correct output and status code', function() {
+  it('Give correct status code', function() {
     request({
-      url: 'http://localhost:7865',
+      url: 'http://localhost:7865/',
       method: 'GET'
     }, (err, res, body) => {
       if (err) {
@@ -12,7 +12,21 @@ describe('API test from `api.js`', function() {
       }
 
       expect(res.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+    it('Give correct output for get', function() {
+    request({
+      url: 'http://localhost:7865/',
+      method: 'GET'
+    }, (err, res, body) => {
+      if (err) {
+        console.log(err);
+      }
+
       expect(body).to.equal('Welcome to the payment system');
+      done();
     });
   });
 });
