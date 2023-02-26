3261536a983d57ac7585a21dd80daaf4c6bb7a52
tommydebisi <tommydebisi@gmail.com>
2023-02-27 00:41:34 +0100

    :seedling: correct use of done

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -2,11 +2,12 @@ const expect = require('chai').expect;
 const getPaymentTokenFromAPI = require('./6-payment_token');
 
 describe('getPaymentTokenFromAPI function', function() {
-  it('Gives correct output when success is True', function() {
+  it('Gives correct output when success is True', function(done) {
     const result = getPaymentTokenFromAPI(true);
 
-    return result.then((val) => {
+    result.then((val) => {
       expect(val.data).to.equal('Successful response from the API');
+      done();
     });
   });
 });
