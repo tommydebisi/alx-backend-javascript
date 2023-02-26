fcd37ab33b25d7afa9cac99092d804dad8e1086d
tommydebisi <tommydebisi@gmail.com>
2023-02-26 17:30:09 +0100

    :construction: async testing

---
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -0,0 +1,19 @@
+const expect = require('chai').expect;
+const getPaymentTokenFromAPI = require('./6-payment_token');
+
+describe('getPaymentTokenFromAPI function', function() {
+  it('Gives correct output when success is True', function() {
+    const result = getPaymentTokenFromAPI(true);
+
+    result.then((val) => {
+      expect(val.data).to.equal('Successful response from the API');
+      done();
+    });
+  });
+
+  it('Gives correct output when success is False', function() {
+    const result = getPaymentTokenFromAPI(false);
+
+    expect(result).to.equal(undefined);
+  });
+});
