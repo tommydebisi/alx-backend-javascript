1623ef5ca5d1abbb17f01e77b64bf4d2c2b8069b
tommydebisi <tommydebisi@gmail.com>
2023-03-15 11:34:26 +0100

    :art: review code

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,52 +1,71 @@
-/* eslint-disable jest/require-hook */
 const express = require('express');
-const { readFile } = require('fs');
+const fs = require('fs');
 
+/**
+ * Create a small HTTP server using Express
+ */
+
+const PORT = 1245;
+const HOST = 'localhost';
+const DATABASE_URI = process.argv.length > 2 ? process.argv[2] : '';
 const app = express();
 
-app.get('/', (req, res) => {
+const countStudents = (path) => new Promise((resolve, reject) => {
+  if (!fs.existsSync(path)) {
+    reject(new Error('Cannot load the database'));
+  }
+  if (!fs.statSync(path).isFile()) {
+    reject(new Error('Cannot load the database'));
+  }
+  const options = { encoding: 'utf-8' };
+  fs.readFile(path, options, (err, data) => {
+    if (err) {
+      reject(new Error('Cannot load the database'));
+    }
+    const rows = data.trim().split('\n');
+    const fields = rows[0].split(',');
+    const fieldPos = fields.findIndex((value) => value === 'field');
+    const fNamePos = fields.findIndex((value) => value === 'firstname');
+
+    const STUDENT_COUNT = Symbol('Student count');
+
+    const map = {};
+    for (const row of rows.slice(1)) {
+      const splitRow = row.split(',');
+      if (splitRow.length && (splitRow.length >= fieldPos + 1 && splitRow.length >= fNamePos + 1)) {
+        const field = splitRow[fieldPos];
+        const firstName = splitRow[fNamePos];
+        map[field] = map[field] || [];
+        map[field].push(firstName);
+        map[STUDENT_COUNT] = (map[STUDENT_COUNT] || 0) + 1;
+      }
+    }
+
+    let result = `Number of students: ${map[STUDENT_COUNT]}`;
+    Object.entries(map).forEach(([field, names]) => {
+      result += `\nNumber of students in ${field}: ${names.length}. List: ${names.join(', ')}`;
+    });
+    resolve(result);
+  });
+});
+
+app.get('/', (_req, res) => {
   res.send('Hello Holberton School!');
 });
 
-app.get('/students', (req, res) => {
-  const text = 'This is the list of our students\n';
-
-  const prom = new Promise((resolve, reject) => {
-    readFile('database.csv', { encoding: 'utf-8', flag: 'r' }, (err, data) => {
-      if (err) {
-        reject(Error('Cannot load the database'));
-      } else {
-        const dList = data.split('\n');
-
-        const cs = [];
-        const swe = [];
-
-        let kList = [];
-        for (const key of dList) {
-          kList = key.split(',');
-          // eslint-disable-next-line no-continue
-          if (kList[0] === 'firstname') { continue; }
-
-          if (kList.slice(-1)[0] === 'CS') {
-            cs.push(kList[0]);
-          } else if (kList.slice(-1)[0] === 'SWE') {
-            swe.push(kList[0]);
-          }
-        }
-
-        const endStr = `Number of students: ${cs.length + swe.length}`;
-        const endSt1 = `Number of students in CS: ${cs.length}. List: ${cs.join(', ')}`;
-        const endSt2 = `Number of students in SWE: ${swe.length}. List: ${swe.join(', ')}`;
-        console.log([endStr, endSt1, endSt2].join('\n'));
-        resolve(`${endStr}\n${endSt1}\n${endSt2}`);
-      }
+app.get('/students', (_req, res) => {
+  let text = 'This is the list of our students\n';
+  countStudents(DATABASE_URI)
+    .then((result) => res.send(text + result))
+    .catch((err) => {
+      text += err instanceof Error ? err.message : err.toString();
+      return res.send(text);
     });
-  });
-
-  prom.then((val) => {
-    console.log(text);
-    res.send(text + val);
-  }).catch(() => res.statusCode(500));
 });
 
-app.listen(1245);
+app.listen(PORT, HOST, () => {
+  console.log(`Server listening on http://${HOST}:${PORT}`);
+  countStudents(DATABASE_URI);
+});
+
+module.exports = app;
