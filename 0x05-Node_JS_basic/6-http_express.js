a720a2b7b2855184ad08c901bd6f649eef50284f
tommydebisi <tommydebisi@gmail.com>
2023-03-15 11:30:06 +0100

    :seedling: reviewed code

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,10 +1,20 @@
 /* eslint-disable jest/require-hook */
 const express = require('express');
 
+/**
+ * Create a small HTTP server using Express
+ */
+
+const PORT = 1245;
+const HOST = 'localhost';
 const app = express();
 
-app.get('/', (req, res) => {
+app.all('/', (_req, res) => {
   res.send('Hello Holberton School!');
 });
 
-app.listen(1245);
+app.listen(PORT, HOST, () => {
+  console.log(`Server listening on http://${HOST}:${PORT}`);
+});
+
+module.exports = app;
