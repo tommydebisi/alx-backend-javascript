a0e5680cbf4f884ae5ef9ba8863632c42ddcd261
tommydebisi <tommydebisi@gmail.com>
2023-03-15 11:21:53 +0100

    :seedling: reviewed code

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,37 +1,47 @@
-const { readFile } = require('fs');
+const fs = require('fs');
 
-function countStudents(file) {
-  return new Promise((resolve, reject) => {
-    readFile(file, { encoding: 'utf-8', flag: 'r' }, (err, data) => {
-      if (err) {
-        reject(Error('Cannot load the database'));
-      } else {
-        const dList = data.split('\n');
+/**
+ * Count the number of students in a database synchronously.
+ * @param {string} path
+ * @return {Promise<number>}
+ */
+const countStudents = (path) => new Promise((resolve, reject) => {
+  if (!fs.existsSync(path)) {
+    reject(new Error('Cannot load the database'));
+  }
+  if (!fs.statSync(path).isFile()) {
+    reject(new Error('Cannot load the database'));
+  }
+  const options = { encoding: 'utf-8' };
+  fs.readFile(path, options, (err, data) => {
+    if (err) {
+      reject(new Error('Cannot load the database'));
+    }
+    const rows = data.trim().split('\n');
+    const fields = rows[0].split(',');
+    const fieldPos = fields.findIndex((value) => value === 'field');
+    const fNamePos = fields.findIndex((value) => value === 'firstname');
 
-        const cs = [];
-        const swe = [];
+    const STUDENT_COUNT = Symbol('Student count');
 
-        let kList = [];
-        for (const key of dList) {
-          kList = key.split(',');
-          // eslint-disable-next-line no-continue
-          if (kList[0] === 'firstname') { continue; }
-
-          if (kList.slice(-1)[0] === 'CS') {
-            cs.push(kList[0]);
-          } else if (kList.slice(-1)[0] === 'SWE') {
-            swe.push(kList[0]);
-          }
-        }
-
-        const endStr = `Number of students: ${cs.length + swe.length}`;
-        const endSt1 = `Number of students in CS: ${cs.length}. List: ${cs.join(', ')}`;
-        const endSt2 = `Number of students in SWE: ${swe.length}. List: ${swe.join(', ')}`;
-        console.log([endStr, endSt1, endSt2].join('\n'));
-        resolve(`${endStr}\n${endSt1}\n${endSt2}`);
+    const map = {};
+    for (const row of rows.slice(1)) {
+      const splitRow = row.split(',');
+      if (splitRow.length && (splitRow.length >= fieldPos + 1 && splitRow.length >= fNamePos + 1)) {
+        const field = splitRow[fieldPos];
+        const firstName = splitRow[fNamePos];
+        map[field] = map[field] || [];
+        map[field].push(firstName);
+        map[STUDENT_COUNT] = (map[STUDENT_COUNT] || 0) + 1;
       }
+    }
+
+    console.log(`Number of students: ${map[STUDENT_COUNT]}`);
+    Object.entries(map).forEach(([field, names]) => {
+      console.log(`Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`);
     });
+    resolve();
   });
-}
+});
 
 module.exports = countStudents;
